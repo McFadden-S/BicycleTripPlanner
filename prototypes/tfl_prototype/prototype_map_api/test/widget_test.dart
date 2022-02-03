18f999c85b87f401944677bcdd2df54e4992c4f0
Linda Melichercikova <linda.melichercikova@kcl.ac.uk>
2022-02-03 10:14:52 +0000

    Add map api prototype

--- prototypes/tfl_prototype/prototype_map_api/test/widget_test.dart
+++ prototypes/tfl_prototype/prototype_map_api/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:prototype_tfl_api/main.dart';
+import 'package:flutter_gmaps/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
