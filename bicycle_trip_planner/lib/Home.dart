c59989b42fa431f2b7d7883be9073878189fc2fd
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-03 23:57:36 +0000

    Add horizontal listView to the bottom of Home screen

--- bicycle_trip_planner/lib/Home.dart
+++ bicycle_trip_planner/lib/Home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -9,35 +8,78 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> stations = [
+    "station 1",
+    "station 2",
+    "station 3",
+    "station 4",
+    "station 5"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: Color(0xff345955),
         child: Container(
-          color: Colors.green,
-          child: Column(
-            children: <Widget> [
-                 Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextField(
-                    onChanged: (input) => { search(input)},
-                    decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(),
-                      labelText: 'Search',
-                    ),
-                  ),
+            padding: EdgeInsets.all(0),
+            height: 56.0,
+            child: ListView.builder(
+                physics: PageScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: stations.length,
+                itemBuilder: (BuildContext context, int index) => stationCard(context, index)
+            )
+        ),
+      ),
+      body: SafeArea(
+          child: Container(
+        color: Colors.green,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (input) => {search(input)},
+                decoration: const InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(),
+                  labelText: 'Search',
                 ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
 
-            ],
-          ),
-        )
+  Widget stationCard(BuildContext context, int index) {
+    return InkWell(
+      onTap: () => stationClicked(index),
+      child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Card(
+                child: Row(
+              children: [
+                Text(stations[index]),
+                Text(" Index" + index.toString()),
+              ],
+                )),
+          )
       ),
     );
   }
 
   void search(String input) {
     print('This method is triggered when search box is changed');
+  }
+
+  void stationClicked(int index) {
+    print("Station of index $index was tapped");
   }
 }
