650c377e5df19d3715ac3a90283e64150904879c
Riccardo Gugliermini <riccardo.gugliermini@kcl.ac.uk>
2022-02-04 12:23:48 +0000

    Implement UI prototype:
    - Define Navigation screen on Navigation.dart
    - Fix ios version to make iOS simulator work

--- bicycle_trip_planner/lib/Navigation.dart
+++ bicycle_trip_planner/lib/Navigation.dart
@@ -15,6 +15,119 @@ class _NavigationState extends State<Navigation> {
       body: SafeArea(
           child: Container(
             color: Colors.blueAccent,
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Spacer(),
+                Card(
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {},
+                    child: SizedBox(
+                      height: 100,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Spacer(),
+                              Icon(
+                                Icons.assistant_direction,
+                                color: Colors.grey[400],
+                                size: 60
+                              ),
+                              const Spacer(),
+                              const Text("Turn left in 1 miles"),
+                              const Spacer(flex: 5),
+                            ],
+                          ),
+                          const Icon(Icons.expand_more)
+                        ],
+                      ),
+                    )
+                  ),
+                ),
+                const Spacer(),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: ()=>{},
+                        child: const Icon(
+                            Icons.location_on
+                        ),
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(10),
+                      ),
+                    )
+                  ]
+                ),
+                const Spacer(),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: ()=>{},
+                        child: const Icon(
+                            Icons.expand
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(10),
+                        ),
+                      )
+                    ]
+                ),
+                const Spacer(),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Card(
+                        child: Container(
+                            padding: const EdgeInsets.all(5.0),
+                            child: const Text("12 : 02")
+                        ),
+                      )
+                    ]
+                ),
+                const Spacer(flex: 50),
+                Row(
+                  children: [
+                    Card(
+                      child:
+                        Container(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Row(
+                            children: [
+                              Column(
+                                  children: const [
+                                    Icon(Icons.timer),
+                                    Text("[ETA]")
+                                  ]
+                              ),
+                              const Text("3.5 miles")
+                            ],
+                          ),
+                        )
+                    ),
+                    const Spacer(flex: 10),
+                    ElevatedButton(
+                        onPressed: ()=>{},
+                        child: const Icon(Icons.pedal_bike)
+                    ),
+                    const Spacer(flex: 1),
+                    ElevatedButton(
+                      onPressed: ()=>{},
+                      child: const Icon(Icons.cancel_outlined)
+                    ),
+                  ],
+                ),
+                const Spacer(),
+              ],
+            ),
           )
       ),
     );
