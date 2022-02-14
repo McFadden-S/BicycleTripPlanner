5fb35317fb2935b43ac311dda9120dae570f273e
Linda Melichercikova <linda.melichercikova@kcl.ac.uk>
2022-02-14 16:29:50 +0000

    Change buttons according to theme

--- bicycle_trip_planner/lib/tryOutStyles.dart
+++ bicycle_trip_planner/lib/tryOutStyles.dart
@@ -14,14 +14,27 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool bike = false;
+
+  Icon directionIcon(String direction) {
+    return Icon(
+        direction.toLowerCase().contains('left') ?
+            Icons.arrow_back :
+        direction.toLowerCase().contains('right') ?
+            Icons.arrow_forward_outlined :
+        direction.toLowerCase().contains('straight') ?
+          Icons.arrow_upward :
+        Icons.circle,
+      color: Colors.blueAccent,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       theme: ThemeData(
         brightness: Brightness.light,
-        primaryColor: const Color(0xFF0C9CEE),
+        primaryColor: Colors.white,
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             textStyle: buttonTextStyle,
@@ -34,21 +47,18 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
           backgroundColor: Colors.white,
           hoverColor: Colors.red,
           elevation: 3,
           splashColor: Colors.transparent,
         ),
-
         cardTheme: CardTheme(
           margin: const EdgeInsets.all(5.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(9.0),
           ),
         ),
-
-
         fontFamily: 'Outfit',
         textTheme: const TextTheme(
           headline6: TextStyle(fontSize: 36.0),
@@ -108,9 +118,33 @@ class _MyAppState extends State<MyApp> {
               Card(
                 child: Container(
                     padding: const EdgeInsets.all(5.0),
-                    child: const Text("12 : 02")
-                ),
+                    child: const Text("12 : 02")),
               ),
+              SizedBox(
+                height: 300,
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 43.0),
+                  child: Card(
+                      color: Colors.white54,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: ListView.separated(
+                          itemCount: 7,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              leading: directionIcon('akfm  adfvga'),
+                              trailing: Text('akfm  adfvga'),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return const Divider();
+                          },
+                        ),
+                      )
+                  ),
+                ),
+              )
             ],
           ),
         ),
--- bicycle_trip_planner/lib/widgets/Navigation.dart
+++ bicycle_trip_planner/lib/widgets/Navigation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/shims/dart_ui_real.dart';
+import 'Directions.dart';
 
 
 class Navigation extends StatefulWidget {
@@ -10,10 +12,15 @@ class Navigation extends StatefulWidget {
 
 class _NavigationState extends State<Navigation> {
   bool extendedNavigation = false;
+  bool cycling = false;
   final List<Widget> entries = [NavigationDirection(), NavigationDirection()];
 
   void setExtendNavigationVied(){
     setState(()=> {extendedNavigation = !extendedNavigation});
+  }
+
+  void setCycling(){
+    setState(()=> {cycling = !cycling});
   }
 
   @override
@@ -25,6 +32,7 @@ class _NavigationState extends State<Navigation> {
             padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 0.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.max,
               children: [
                 const Spacer(),
                 Card(
@@ -37,7 +45,7 @@ class _NavigationState extends State<Navigation> {
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Container(
-                            margin: EdgeInsets.only(top: 10),
+                            margin: const EdgeInsets.only(top: 10),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
@@ -62,7 +70,8 @@ class _NavigationState extends State<Navigation> {
                               ),
                             )
                           :
-                            const Icon(Icons.expand_more)
+                            const Icon(Icons.expand_more),
+                          extendedNavigation ? const Icon(Icons.expand_less) : const SizedBox.shrink(),
                         ],
                       ),
                     )
@@ -72,15 +81,11 @@ class _NavigationState extends State<Navigation> {
                 Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      ElevatedButton(
+                      FloatingActionButton(
                         onPressed: () => {},
                         child: const Icon(
                             Icons.location_on
                         ),
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(10),
-                      ),
                     )
                   ]
                 ),
@@ -88,14 +93,10 @@ class _NavigationState extends State<Navigation> {
                 Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      ElevatedButton(
+                      FloatingActionButton(
                         onPressed: ()=>{},
                         child: const Icon(
                             Icons.expand
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          shape: const CircleBorder(),
-                          padding: const EdgeInsets.all(10),
                         ),
                       )
                     ]
@@ -105,14 +106,26 @@ class _NavigationState extends State<Navigation> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       Card(
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                              color: Color(0xFF8B0000),
+                              width: 1
+                          ),
+                          borderRadius: BorderRadius.circular(9.0),
+                        ),
                         child: Container(
                             padding: const EdgeInsets.all(5.0),
-                            child: const Text("12 : 02")
+                            child: const Text(
+                                "12 : 02",
+                              style: TextStyle(
+                                color: Color(0xFF8B0000),
+                              ),
+                            )
                         ),
                       )
                     ]
                 ),
-                const Spacer(flex: 50),
+                Spacer(flex: 50),
                 Row(
                   children: [
                     Card(
@@ -132,15 +145,36 @@ class _NavigationState extends State<Navigation> {
                           ),
                         )
                     ),
-                    const Spacer(flex: 10),
-                    ElevatedButton(
-                        onPressed: ()=>{},
-                        child: const Icon(Icons.pedal_bike)
-                    ),
                     const Spacer(flex: 1),
                     ElevatedButton(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.directions_walk,
+                            color: cycling ? Colors.white70 : Colors.white,
+                            size: 30,
+                          ),
+                          const Text('/', style: TextStyle(fontSize: 25),),
+                          Icon(
+                            Icons.directions_bike,
+                            color: cycling ? Colors.white : Colors.white70,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        setCycling();
+                      },
+                    ),
+                    const Spacer(flex: 10),
+                    FloatingActionButton(
                       onPressed: ()=>{},
-                      child: const Icon(Icons.cancel_outlined)
+                        backgroundColor: Colors.red,
+                      child: const Icon(
+                        Icons.cancel_outlined,
+                        color: Colors.white,
+                        size: 35,
+                      )
                     ),
                   ],
                 ),
