6c54f9b38b4f0427f98ae017e80dd2f5802d0231
Linda Melichercikova <linda.melichercikova@kcl.ac.uk>
2022-02-14 01:26:50 +0000

    Initial buttons style

---
+++ bicycle_trip_planner/lib/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+const kPrimaryColor = Color(0xFF0C9CEE);
+const buttonPrimaryColor = Color(0xFF0C9CEE);
+const kPrimaryLightColor = Color(0xFFBBDEFB);
+const mainFontColor = Color(0xFF1C1C1C);
+const secondaryFontColor = Color(0xFF605D5D);
+
+const buttonTextStyle = TextStyle(
+  color: Color(0xFFFFFFFF),
+  fontFamily: 'Outfit',
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+//const buttonStyle = ButtonStyle(
+//  textStyle: MaterialStateProperty.all(buttonTextStyle),
+//  backgroundColor: MaterialStateProperty.all(const Color(0xFF0C9CEE)),
+//  splashFactory:
+//);
---
+++ bicycle_trip_planner/lib/tryOutStyles.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: const Color(0xFF0C9CEE),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            textStyle: buttonTextStyle,
+            padding: EdgeInsets.fromLTRB(15, 8, 15, 8),
+            primary: Colors.red[700],
+            shadowColor: Colors.grey,
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(13.0),
+            ),
+          ),
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.white,
+          hoverColor: Colors.red,
+          elevation: 3,
+          splashColor: Colors.transparent,
+        ),
+        fontFamily: 'Outfit',
+        textTheme: const TextTheme(
+          headline6: TextStyle(fontSize: 36.0),
+          button: TextStyle(fontSize: 18.0),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              ElevatedButton(
+                child: Text(
+                  'Hello Worlds',
+                ),
+                onPressed: () {
+                  print("Hi");
+                },
+              ),
+              FloatingActionButton(onPressed: () => print('float')),
+              ElevatedButton(
+                child: Icon(
+                  Icons.directions_bike,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  print("Go");
+                },
+              ),
+              ElevatedButton(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.directions_walk,
+                      color: Colors.white,
+                    ),
+                    Text('/'),
+                    Icon(
+                      Icons.directions_bike,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  print("Go");
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
