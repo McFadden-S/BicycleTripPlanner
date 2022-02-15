a27de09e6fd2232659020f33c68721edb585c857
Edward Reamer <edward.reamer@kcl.ac.uk>
2022-02-15 23:47:55 +0000

    Update Map.dart
    Add necessary import

--- bicycle_trip_planner/lib/widgets/Map.dart
+++ bicycle_trip_planner/lib/widgets/Map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bicycle_trip_planner/models/station.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
