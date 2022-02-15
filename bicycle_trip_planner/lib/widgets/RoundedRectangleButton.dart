659ed89ba5ffca643050e606b341fea2e776be44
Edward Reamer <edward.reamer@kcl.ac.uk>
2022-02-15 19:56:19 +0000

    Create RoundedRectangleButton.dart
    Create rounded rectangle button blueprint and add styling for it

---
+++ bicycle_trip_planner/lib/widgets/RoundedRectangleButton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class RoundedRectangleButton extends StatefulWidget {
+
+  final IconData iconIn;
+  final VoidCallback onButtonClicked;
+  final Color buttonColor;
+
+  const RoundedRectangleButton({ Key? key, required this.iconIn, required this.onButtonClicked, required this.buttonColor}) : super(key: key);
+
+  @override
+  _StationCardState createState() => _StationCardState();
+}
+
+class _StationCardState extends State<RoundedRectangleButton> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ElevatedButton(
+      onPressed: widget.onButtonClicked,
+      child: Icon(
+          widget.iconIn,
+          color: Colors.white,
+      ),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(17.0),
+        ),
+        padding: const EdgeInsets.all(10),
+        primary: widget.buttonColor,
+      ),
+    );
+  }
+}
