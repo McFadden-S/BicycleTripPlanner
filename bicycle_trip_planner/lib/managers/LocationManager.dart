98706af509865ce89e7d5707204be437a84acb9a
Shae McFadden <shae.mcfadden@kcl.ac.uk>
2022-02-16 20:02:19 +0000

    Abstract Location from station card
    Co-Authored-By: Sean Pan <sean.pan@kcl.ac.uk>

--- bicycle_trip_planner/lib/managers/LocationManager.dart
+++ bicycle_trip_planner/lib/managers/LocationManager.dart
@@ -7,14 +7,16 @@ class LocationManager{
   // Defines how the location should be fine-tuned
   // ignore: prefer_const_constructors
   final LocationSettings locationSettings = LocationSettings(
-    accuracy: LocationAccuracy.high, // How accurate the location is
-    distanceFilter: 0, // The distance needed to travel until the next update (0 means it will always update)
+    accuracy: LocationAccuracy.best, // How accurate the location is
+    distanceFilter: 25, // The distance needed to travel until the next update (0 means it will always update)
   );
 
   // This is specifying the Locator class in locator.dart
   final Locator _locator = Locator();
 
   LocationManager();
+
+  //********** public *********
 
   Future<LatLng> locate() async{
     return _locator.locate();
@@ -23,5 +25,26 @@ class LocationManager{
   Future<LocationPermission> requestPermission() async{
     return await Geolocator.requestPermission();
   }
+
+  Future<double> distanceTo(LatLng pos) async {
+    return distanceFromTo(await locate(), pos);
+  }
+
+  double distanceFromTo(LatLng posFrom, LatLng posTo){
+    return _convertMetresToMiles(_calculateDistance(posFrom, posTo));
+  }
+
+  //********** private *********
+
+  double _convertMetresToMiles(double distance) {
+    return distance * 0.000621;
+  }
+
+  // Returns the distance between the two points in metres
+  double _calculateDistance(LatLng pos1, LatLng pos2) {
+    return Geolocator.distanceBetween(
+        pos1.latitude, pos1.longitude, pos2.latitude, pos2.longitude);
+  }
+
 
 }
