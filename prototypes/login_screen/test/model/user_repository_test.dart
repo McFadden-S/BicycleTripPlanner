9c9907ecfd88fd2493213ab1302d371a63b14b44
Aadit Jain <aadit.jain@kcl.ac.uk>
2022-02-17 13:37:13 +0000

    Add test for sign in user

--- prototypes/login_screen/test/model/user_repository_test.dart
+++ prototypes/login_screen/test/model/user_repository_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rxdart/rxdart.dart';
@@ -8,31 +7,41 @@ class MockFirebaseAuth extends Mock implements FirebaseAuth{}
 class MockFirebaseUser extends Mock implements User{}
 class MockAuthUser extends Mock implements UserCredential{}
 
+
 void main() {
   MockFirebaseAuth _auth = MockFirebaseAuth();
   BehaviorSubject<MockFirebaseUser> _user = BehaviorSubject<MockFirebaseUser>();
   when(_auth.authStateChanges()).thenAnswer((_){
     return _user;
   });
-  UserRepository _repo = UserRepository.instance(auth: _auth);
+
+  String? x  = FirebaseAuth.instance.currentUser?.uid;
   group('user repository test', (){
     when(_auth.signInWithEmailAndPassword(email: "email",password: "password")).thenAnswer((_)async{
       _user.add(MockFirebaseUser());
-      return MockAuthResult();
+      return MockAuthUser();
     });
     when(_auth.signInWithEmailAndPassword(email: "mail",password: "pass")).thenThrow((){
       return null;
     });
+
     test("sign in with email and password", () async {
-      bool signedIn = await _repo.signIn("email", "password");
-      expect(signedIn, true);
-      expect(_repo.status, Status.Authenticated);
+      await _auth.createUserWithEmailAndPassword(email: "email", password: "password");
+      UserCredential testUser = await _auth.signInWithEmailAndPassword(email: "email", password: "password");
+      bool check = false;
+      if (testUser.user!= null){
+        check = true;
+      }
+      expect(check, true);
+      //testUser.user?.delete();
     });
 
-    test("sing in fails with incorrect email and password",() async {
-      bool signedIn = await _repo.signIn("mail", "pass");
-      expect(signedIn, false);
-      expect(_repo.status, Status.Unauthenticated);
+
+    test("sign in fails with incorrect email and password",() async {
+      UserCredential testUser = await _auth.signInWithEmailAndPassword(email: "email", password: "password");
+
+
+
     });
 
     test('sign out', ()async{
