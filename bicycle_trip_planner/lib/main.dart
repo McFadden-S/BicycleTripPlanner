8dfdfbe28af7e2283248f6388b1a609300fdacae
Aadit Jain <aadit.jain@kcl.ac.uk>
2022-02-18 11:50:01 +0000

    Add check for multiple firebase apps

--- bicycle_trip_planner/lib/main.dart
+++ bicycle_trip_planner/lib/main.dart
@@ -12,9 +12,11 @@ import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (Firebase.apps.isEmpty){
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(
       ChangeNotifierProvider(
           create: (context) => ApplicationBloc(),
