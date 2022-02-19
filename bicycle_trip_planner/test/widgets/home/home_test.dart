9c8327de187cfcb1f259a5716cf8118906faf394
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-19 16:40:50 +0000

    Add widget test for search box in home screen

--- bicycle_trip_planner/test/widgets/home/home_test.dart
+++ bicycle_trip_planner/test/widgets/home/home_test.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
 import 'package:bicycle_trip_planner/main.dart';
+import 'package:bicycle_trip_planner/widgets/general/Search.dart';
+import 'package:bicycle_trip_planner/widgets/home/Home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-
 
 Widget buildWidget(Widget widgetIn){
   return MultiProvider(
@@ -18,8 +21,22 @@ Widget buildWidget(Widget widgetIn){
 
 
 void main() {
+  setUpAll(() async {
+    HttpOverrides.global = null;
+  });
+
   testWidgets('Navigation has home', (WidgetTester tester) async {
     await tester.pumpWidget(buildWidget(MyApp()));
     expect(find.text('Home'), findsOneWidget);
+  });
+
+
+
+  testWidgets("Home has search box", (WidgetTester tester) async {
+    await tester.pumpWidget(buildWidget(MaterialApp(home: Home())));
+
+    final searchBox = find.byType(Search);
+
+    expect(searchBox, findsOneWidget);
   });
 }
