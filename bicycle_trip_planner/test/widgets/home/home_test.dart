9a11122c8a8d45cf0da486f2d11361f300d31af4
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-19 16:57:04 +0000

    Add tests for home screen
    - Fix test compilation error by merging with branch 71
    - Add widget tests for home.dart

--- bicycle_trip_planner/test/widgets/home/home_test.dart
+++ bicycle_trip_planner/test/widgets/home/home_test.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
 import 'package:bicycle_trip_planner/main.dart';
+import 'package:bicycle_trip_planner/widgets/general/MapWidget.dart';
 import 'package:bicycle_trip_planner/widgets/general/Search.dart';
 import 'package:bicycle_trip_planner/widgets/home/Home.dart';
+import 'package:bicycle_trip_planner/widgets/home/StationBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
@@ -30,13 +32,26 @@ void main() {
     expect(find.text('Home'), findsOneWidget);
   });
 
-
-
-  testWidgets("Home has search box", (WidgetTester tester) async {
+  testWidgets("Home has a safeArea", (WidgetTester tester) async {
     await tester.pumpWidget(buildWidget(MaterialApp(home: Home())));
-
-    final searchBox = find.byType(Search);
-
-    expect(searchBox, findsOneWidget);
+    expect(find.byType(SafeArea), findsOneWidget);
   });
+
+  testWidgets("Home has a search box", (WidgetTester tester) async {
+    await tester.pumpWidget(buildWidget(MaterialApp(home: Home())));
+    expect(find.widgetWithText(Search, 'Search'), findsOneWidget);
+  });
+
+  testWidgets("Home has a map", (WidgetTester tester) async {
+    await tester.pumpWidget(buildWidget(MaterialApp(home: Home())));
+    expect(find.byType(MapWidget), findsOneWidget);
+  });
+
+  testWidgets("Home has a bottom StationBar", (WidgetTester tester) async {
+    await tester.pumpWidget(buildWidget(MaterialApp(home: Home())));
+    expect(find.byType(StationBar), findsOneWidget);
+  });
+
+
+
 }
