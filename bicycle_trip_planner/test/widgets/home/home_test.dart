cb92cda1bbf4fdfa0f1ae2e9c02e3db4e769a931
Riccardo Gugliermini <riccardo.gugliermini@kcl.ac.uk>
2022-02-19 12:10:49 +0000

    Add test to home_test

--- bicycle_trip_planner/test/widgets/home/home_test.dart
+++ bicycle_trip_planner/test/widgets/home/home_test.dart
@@ -1,16 +1,21 @@
-import 'package:bicycle_trip_planner/widgets/general/Search.dart';
-import 'package:bicycle_trip_planner/widgets/home/Home.dart';
-import 'package:flutter/material.dart';
+import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
+import 'package:bicycle_trip_planner/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+buildMyApp() {
+  return ChangeNotifierProvider(
+      create: (context) => ApplicationBloc(),
+      child: const MyApp()
+  );
+}
+
 
 void main() {
-  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Home()));
-    //
-    // final searchWidget = find.byWidget(Search(labelTextIn:'Search', searchController: TextEditingController()));
-    //
-    // expect(searchWidget, findsOneWidget);
+  testWidgets('Navigation has home', (WidgetTester tester) async {
+    await tester.pumpWidget(buildMyApp());
+
+    expect(find.text('Home'), findsOneWidget);
 
   });
 }
-
