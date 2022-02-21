980b89679d702c14814b1ebbb8cffe154baa668c
michaelmulianto <53204221+michaelmulianto@users.noreply.github.com>
2022-02-21 14:17:38 -0800

    add camera manager test

--- bicycle_trip_planner/test/marker_manager_test.dart
+++ bicycle_trip_planner/test/marker_manager_test.dart
@@ -9,12 +9,12 @@ import 'package:bicycle_trip_planner/models/station.dart';
 
 
 void main(){
+  final marker = MarkerManager();
   test('ensure that there are no markers at the start', (){
     expect(MarkerManager().getMarkers().length,0);
   });
 
   test('ensure marker is added when requested', (){
-    final marker = MarkerManager();
     expect(marker.getMarkers().length,0);
     marker.setMarker(LatLng(51.511448, -0.116414));
     expect(marker.getMarkers().length,1);
@@ -22,7 +22,6 @@ void main(){
   });
 
   test('ensure marker is added for place requested', (){
-    final marker = MarkerManager();
     Location location = Location(lat: 51.511448, lng: -0.116414);
     Geometry geometry = Geometry(location: location);
     Place place = Place(geometry: geometry, name: 'Bush House');
@@ -32,6 +31,19 @@ void main(){
     marker.getMarkers().clear();
   });
 
-
+  // test('ensure marker is added for user location', (){
+  //   final marker = MarkerManager();
+  //   Position point = Position(longitude: 51.511448, latitude: -0.116414, timestamp: timestamp, accuracy: accuracy, altitude: altitude, heading: heading, speed: speed, speedAccuracy: speedAccuracy);
+  //   expect(marker.getMarkers().length,0);
+  //   marker.setUserMarker(point);
+  //   expect(marker.getMarkers().length,0);
+  //   marker.getMarkers().clear();
+  // });
+  //
+  // test('ensure marker for station is correct', (){
+  //   final marker = MarkerManager();
+  //   Station station = Station(id: 1, name: 'Holborn Station', lat: 0.0, long: 0.0, bikes: 10, emptyDocks: 2, totalDocks: 8);
+  //   expect(marker.getStationMarker(station), Marker(markerId: MarkerId("Holborn Station"), infoWindow: InfoWindow(title: 'Holborn Station')));
+  // });
 
 }
