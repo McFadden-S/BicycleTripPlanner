5a876440569e8bb7eecb8ee578adf2a9b62ccb1b
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-22 22:17:32 +0000

    Restyle group button

--- bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
+++ bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
@@ -23,7 +23,7 @@ class _RoutePlanningState extends State<RoutePlanning> {
   Widget build(BuildContext context) {
 
     final applicationBloc = Provider.of<ApplicationBloc>(context);
-    String dropdownValue = 'One';
+    int groupSizeValue = 1;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -50,27 +50,38 @@ class _RoutePlanningState extends State<RoutePlanning> {
                 const Spacer(),
                 Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                   //TODO: an empty function is being passed here for now
-                  DropdownButton<String>(
-                    value: dropdownValue,
-                    icon: const Icon(Icons.group),
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.deepPurple),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
+                  Padding(
+                    padding: const EdgeInsets.only(right: 5.0, top: 10.0),
+                    child: Container(
+                      padding: const EdgeInsets.only(right: 2.0, left: 5.0),
+                      decoration: const BoxDecoration(
+                          color: const Color.fromRGBO(12, 156, 238, 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: DropdownButton<int>(
+                          isDense: true,
+                          value: groupSizeValue,
+                          icon: const Icon(Icons.group, color: Colors.white),
+                          iconSize: 30,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.white, fontSize: 18),
+                          onChanged: (int? newValue) {
+                            setState(() {
+                              groupSizeValue = newValue!;
+                            });
+                          },
+                          items: <int>[1, 2, 3, 4, 6, 7, 8, 9, 10]
+                              .map<DropdownMenuItem<int>>((int value) {
+                            return DropdownMenuItem<int>(
+                              value: value,
+                              child: Text(value.toString()),
+                            );
+                          }).toList(),
+                        ),
+                      ),
                     ),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = newValue!;
-                      });
-                    },
-                    items: <String>['One', 'Two', 'Free', 'Four']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
                   )
                 ]),
                 const Spacer(flex: 50),
