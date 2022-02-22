c2c8c888479508765060d703f81e8a58e6ee9ad3
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-22 21:34:42 +0000

    Changed group button to DropdownButton

--- bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
+++ bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
@@ -23,6 +23,7 @@ class _RoutePlanningState extends State<RoutePlanning> {
   Widget build(BuildContext context) {
 
     final applicationBloc = Provider.of<ApplicationBloc>(context);
+    String dropdownValue = 'One';
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -34,10 +35,10 @@ class _RoutePlanningState extends State<RoutePlanning> {
               children: [
                 const Spacer(),
                 Stack(
-                  children: [RouteCard(
-                    startSearchController: startSearchController,
-                    destinationSearchController: destinationSearchController,
-                  ),]
+                    children: [RouteCard(
+                      startSearchController: startSearchController,
+                      destinationSearchController: destinationSearchController,
+                    ),]
                 ),
                 const Spacer(),
                 Row(mainAxisAlignment: MainAxisAlignment.end, children: [
@@ -49,9 +50,28 @@ class _RoutePlanningState extends State<RoutePlanning> {
                 const Spacer(),
                 Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                   //TODO: an empty function is being passed here for now
-                  CircleButton(
-                      iconIn: Icons.group,
-                      onButtonClicked: () => {print("group icon")})
+                  DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: const Icon(Icons.group),
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownValue = newValue!;
+                      });
+                    },
+                    items: <String>['One', 'Two', 'Free', 'Four']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  )
                 ]),
                 const Spacer(flex: 50),
                 Row(mainAxisAlignment: MainAxisAlignment.end, children: [
@@ -66,7 +86,7 @@ class _RoutePlanningState extends State<RoutePlanning> {
                             destinationSearchController.text
                         );
                       }
-                      )
+                  )
                 ]),
               ],
             ),
