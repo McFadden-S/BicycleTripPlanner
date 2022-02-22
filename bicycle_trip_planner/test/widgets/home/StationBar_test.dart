ad3e2ce72abcf54862d97ab41bc10afbf4bcea53
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-22 21:05:43 +0000

    Refactor home_test.dart and StationBar_test.dart
    - Use SetUp.dart to pumpWidget in home test files
    
    closes #99

--- bicycle_trip_planner/test/widgets/home/StationBar_test.dart
+++ bicycle_trip_planner/test/widgets/home/StationBar_test.dart
@@ -1,22 +1,9 @@
 import 'dart:io';
 
-import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
 import 'package:bicycle_trip_planner/widgets/home/StationBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-
-Widget buildWidget(Widget widgetIn){
-  return MultiProvider(
-    providers: [
-      ListenableProvider(create: (context) => ApplicationBloc()),
-    ],
-    builder: (context, child) {
-      return widgetIn;
-    },
-  );
-}
-
+import '../../setUp.dart';
 
 void main() {
   setUpAll(() async {
@@ -24,22 +11,22 @@ void main() {
   });
 
   testWidgets("StationBar has a title 'Nearby Stations'", (WidgetTester tester) async {
-    await tester.pumpWidget(buildWidget(MaterialApp(home: Material(child:StationBar()))));
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
     expect(find.text('Nearby Stations'), findsOneWidget);
   });
 
   testWidgets("StationBar has an icon button 'firstPage'", (WidgetTester tester) async {
-    await tester.pumpWidget(buildWidget(MaterialApp(home: Material(child:StationBar()))));
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
     expect(find.widgetWithIcon(IconButton, Icons.first_page), findsOneWidget);
   });
 
   testWidgets("StationBar has an icon button 'expanded station list'", (WidgetTester tester) async {
-    await tester.pumpWidget(buildWidget(MaterialApp(home: Material(child:StationBar()))));
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
     expect(find.widgetWithIcon(IconButton, Icons.menu), findsOneWidget);
   });
 
   testWidgets("StationBar has a stations listView", (WidgetTester tester) async {
-    await tester.pumpWidget(buildWidget(MaterialApp(home: Material(child:StationBar()))));
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
     expect(find.byType(ListView), findsOneWidget);
   });
 
