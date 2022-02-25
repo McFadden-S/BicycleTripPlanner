aeffccb897344db0aa1ca2742b0473f58c1e95dd
Sean Pan <sean.pan@kcl.ac.uk>
2022-02-25 21:03:22 +0000

    ClearChanged in if statement

--- bicycle_trip_planner/lib/widgets/general/Search.dart
+++ bicycle_trip_planner/lib/widgets/general/Search.dart
@@ -56,6 +56,7 @@ class _SearchState extends State<Search> {
 
     if(!isSearching && routeManager.ifChanged()){
       widget.searchController.text = getText();
+      routeManager.clearChanged();
     }
 
     return Stack(
