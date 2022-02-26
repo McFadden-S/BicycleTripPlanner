6733aa1550ccbf77a4770c7335f62e6dbc4bbb4d
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-02-26 21:19:30 +0000

    Prepare RoutePlanning layout to be fixed
    (Old Previous code commented due to inconsistency)

--- bicycle_trip_planner/lib/widgets/navigation/Navigation.dart
+++ bicycle_trip_planner/lib/widgets/navigation/Navigation.dart
@@ -133,51 +133,6 @@ class _NavigationState extends State<Navigation> {
                 ],
               ),
           ),
-          // Container(
-          //   // color: Colors.green,
-          //   child: LimitedBox(
-          //     maxHeight: MediaQuery.of(context).size.height * 0.1,
-          //     child: Row(
-          //       mainAxisSize: MainAxisSize.max,
-          //       // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //       children: [
-          //         Expanded(flex: 1,child: const DistanceETACard()),
-          //         Expanded(
-          //           flex: 1,
-          //           child: Column(
-          //             children: [
-          //               WalkOrCycleToggle(directionManager: directionManager),
-          //               TextButton(
-          //                   onPressed: () => applicationBloc.endRoute(),
-          //                   child: Text("End",
-          //                       style: TextStyle(color: Colors.white)),
-          //                   style: ButtonStyle(
-          //                       padding:
-          //                       MaterialStateProperty.all<EdgeInsets>(
-          //                           EdgeInsets.all(15)),
-          //                       shape: MaterialStateProperty.all<
-          //                           RoundedRectangleBorder>(
-          //                           RoundedRectangleBorder(
-          //                               borderRadius:
-          //                               BorderRadius.circular(18.0),
-          //                               side: BorderSide(color: Colors.red))),
-          //                       backgroundColor:
-          //                       MaterialStateProperty.all<Color>(
-          //                           Colors.red))),
-          //             ],
-          //           ),
-          //         )
-          //         // CircleButton(
-          //         //     iconIn: Icons.cancel_outlined,
-          //         //     onButtonClicked: () {
-          //         //       applicationBloc.endRoute();
-          //         //     },
-          //         //     buttonColor: Colors.red
-          //         // ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
         ],
       ),
     );
--- bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
+++ bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
@@ -24,92 +24,100 @@ class _RoutePlanningState extends State<RoutePlanning> {
     return SafeArea(
       child: Stack(
         children: [
-          Column(
-            children: [
-              const Spacer(),
-              Stack(children: [RouteCard()]),
-              const Spacer(),
-              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
-                  child: CurrentLocationButton(),
-                )
-              ]),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 5.0, top: 10.0),
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 3.0, right: 7.0),
-                    decoration: const BoxDecoration(
-                      color: const Color.fromRGBO(12, 156, 238, 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: DropdownButton<int>(
-                        isDense: true,
-                        value: groupSizeValue,
-                        icon: const Icon(Icons.group, color: Colors.white),
-                        iconSize: 30,
-                        elevation: 16,
-                        style: const TextStyle(
-                            color: Colors.black45, fontSize: 18),
-                        menuMaxHeight: 200,
-                        onChanged: (int? newValue) =>
-                            onGroupSizeChanged(newValue!),
-                        selectedItemBuilder: (BuildContext context) {
-                          return groupSizeOptions.map((int value) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 5.0),
-                              child: Text(
-                                groupSizeValue.toString(),
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                            );
-                          }).toList();
-                        },
-                        items: groupSizeOptions
-                            .map<DropdownMenuItem<int>>((int value) {
-                          return DropdownMenuItem<int>(
-                            value: value,
-                            child: Text(value.toString()),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  )
-                ),
-                ]
-                ),
-                const Spacer(flex: 50),
-                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  const DistanceETACard(),
-                  const Spacer(flex: 10),
-                  RoundedRectangleButton(
-                      iconIn: Icons.directions_bike,
-                      buttonColor: Colors.green,
-                      onButtonClicked: () {
-                        if(RouteManager().ifStartSet() && RouteManager().ifDestinationSet()){ 
-                          applicationBloc.setSelectedScreen('navigation');
-                          applicationBloc.showBackButton = false;
-                        }
-                        else{
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                            content: Text("Start and Destination have not been set!"),
-                          ));
-                        }
-                      }
-                  )
-                ]),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                RouteCard(),
               ],
             ),
-          ],
-        ),
-      );
+          )
+          // Column(
+          //   children: [
+          //     const Spacer(),
+          //     Stack(children: [RouteCard()]),
+          //     const Spacer(),
+          //     Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          //       Padding(
+          //         padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+          //         child: CurrentLocationButton(),
+          //       )
+          //     ]),
+          //     const SizedBox(height: 10),
+          //     Row(
+          //       mainAxisAlignment: MainAxisAlignment.end,
+          //       children: [
+          //       Padding(
+          //         padding: const EdgeInsets.only(right: 5.0, top: 10.0),
+          //         child: Container(
+          //           padding: const EdgeInsets.only(left: 3.0, right: 7.0),
+          //           decoration: const BoxDecoration(
+          //             color: const Color.fromRGBO(12, 156, 238, 1.0),
+          //             borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          //           ),
+          //           child: Padding(
+          //             padding: const EdgeInsets.all(2.0),
+          //             child: DropdownButton<int>(
+          //               isDense: true,
+          //               value: groupSizeValue,
+          //               icon: const Icon(Icons.group, color: Colors.white),
+          //               iconSize: 30,
+          //               elevation: 16,
+          //               style: const TextStyle(
+          //                   color: Colors.black45, fontSize: 18),
+          //               menuMaxHeight: 200,
+          //               onChanged: (int? newValue) =>
+          //                   onGroupSizeChanged(newValue!),
+          //               selectedItemBuilder: (BuildContext context) {
+          //                 return groupSizeOptions.map((int value) {
+          //                   return Padding(
+          //                     padding:
+          //                         const EdgeInsets.symmetric(horizontal: 5.0),
+          //                     child: Text(
+          //                       groupSizeValue.toString(),
+          //                       style: const TextStyle(color: Colors.white),
+          //                     ),
+          //                   );
+          //                 }).toList();
+          //               },
+          //               items: groupSizeOptions
+          //                   .map<DropdownMenuItem<int>>((int value) {
+          //                 return DropdownMenuItem<int>(
+          //                   value: value,
+          //                   child: Text(value.toString()),
+          //                 );
+          //               }).toList(),
+          //             ),
+          //           ),
+          //         )
+          //       ),
+          //       ]
+          //       ),
+          //       // const Spacer(flex: 50),
+          //       // Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          //       //   const DistanceETACard(),
+          //       //   const Spacer(flex: 10),
+          //       //   RoundedRectangleButton(
+          //       //       iconIn: Icons.directions_bike,
+          //       //       buttonColor: Colors.green,
+          //       //       onButtonClicked: () {
+          //       //         if(RouteManager().ifStartSet() && RouteManager().ifDestinationSet()){
+          //       //           applicationBloc.setSelectedScreen('navigation');
+          //       //           applicationBloc.showBackButton = false;
+          //       //         }
+          //       //         else{
+          //       //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          //       //             content: Text("Start and Destination have not been set!"),
+          //       //           ));
+          //       //         }
+          //       //       }
+          //       //   )
+          //       // ]),
+          //     ],
+          //   ),
+        ],
+      ),
+    );
   }
 
   // TODO: remove print statement after linking the button correctly
