11d8b83ba8727ddf8f9e2d4124262e2428f68e1c
Sean Pan <sean.pan@kcl.ac.uk>
2022-03-01 04:22:27 +0000

    Set home search to destination on tap

--- bicycle_trip_planner/lib/widgets/home/HomeWidgets.dart
+++ bicycle_trip_planner/lib/widgets/home/HomeWidgets.dart
@@ -1,10 +1,8 @@
-import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
-import 'package:bicycle_trip_planner/models/search_types.dart';
+import 'package:bicycle_trip_planner/managers/RouteManager.dart';
 import 'package:bicycle_trip_planner/widgets/general/currentLocationButton.dart';
 import 'package:flutter/material.dart';
 import 'package:bicycle_trip_planner/widgets/general/Search.dart';
 import 'package:bicycle_trip_planner/widgets/home/StationBar.dart';
-import 'package:provider/provider.dart';
 
 class HomeWidgets extends StatefulWidget {
   const HomeWidgets({Key? key}) : super(key: key);
@@ -14,6 +12,8 @@ class HomeWidgets extends StatefulWidget {
 }
 
 class _HomeWidgetsState extends State<HomeWidgets> {
+
+  final RouteManager routeManager = RouteManager(); 
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +35,7 @@ class _HomeWidgetsState extends State<HomeWidgets> {
                         child: Search(
                           labelTextIn: 'Search',
                           searchController: TextEditingController(),
+                          uid: routeManager.getDestination().getUID(),
                         ),
                       ),
                       GestureDetector(
