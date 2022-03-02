92048108c8bbf1ac1ed3531675734ae9025ab19d
michaelmulianto <53204221+michaelmulianto@users.noreply.github.com>
2022-03-02 13:19:55 +0800

    create search for city function

--- bicycle_trip_planner/lib/widgets/weather/weather.dart
+++ bicycle_trip_planner/lib/widgets/weather/weather.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Weather extends StatefulWidget {
   const Weather({Key? key}) : super(key: key);
@@ -9,6 +10,22 @@ class Weather extends StatefulWidget {
 }
 
 class _WeatherState extends State<Weather> {
+
+  String location = 'London';
+  int woeid = 44418;
+  String searchApiUrl = 'https://www.metaweather.com/api/location/search/?query=london';
+  String locationApiUrl = 'https://www.metaweather.com/api/location/';
+
+  void fetchSearch(String city) async {
+    var searchResult = await http.get(Uri.parse(searchApiUrl + city));
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result["title"];
+      woeid = result["woeid"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
