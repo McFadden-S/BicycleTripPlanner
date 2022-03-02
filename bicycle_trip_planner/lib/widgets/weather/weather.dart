3d6511962ca8d1df57929be7d887539bc61589dd
michaelmulianto <53204221+michaelmulianto@users.noreply.github.com>
2022-03-02 13:10:17 +0800

    set up files and imports for weather

---
+++ bicycle_trip_planner/lib/widgets/weather/weather.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Weather extends StatefulWidget {
+  const Weather({Key? key}) : super(key: key);
+
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
