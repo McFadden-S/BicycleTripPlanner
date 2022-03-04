f7c4abcf18142450df43f5f2ea50f97c4eb5ce40
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-03-04 20:42:56 +0000

    Add group size selector to a separate file
    - Create GroupSizeSelector.dart
    - Add group size selector to home widgets

--- bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
+++ bicycle_trip_planner/lib/widgets/routeplanning/RoutePlanning.dart
@@ -2,6 +2,7 @@ import 'package:bicycle_trip_planner/bloc/application_bloc.dart';
 import 'package:bicycle_trip_planner/managers/DirectionManager.dart';
 import 'package:bicycle_trip_planner/managers/RouteManager.dart';
 import 'package:bicycle_trip_planner/widgets/general/CircleButton.dart';
+import 'package:bicycle_trip_planner/widgets/general/GroupSizeSelector.dart';
 import 'package:bicycle_trip_planner/widgets/general/ViewRouteButton.dart';
 import 'package:wakelock/wakelock.dart';
 import 'package:bicycle_trip_planner/widgets/general/DistanceETACard.dart';
@@ -26,8 +27,6 @@ class _RoutePlanningState extends State<RoutePlanning> {
   final RouteManager _routeManager = RouteManager();
   final DirectionManager _directionManager = DirectionManager();
 
-  List<int> groupSizeOptions = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
   @override
   Widget build(BuildContext context) {
     final applicationBloc = Provider.of<ApplicationBloc>(context);
@@ -51,48 +50,7 @@ class _RoutePlanningState extends State<RoutePlanning> {
                           SizedBox(height: 10),
                           ViewRouteButton(),
                           SizedBox(height: 10),
-                          Container(
-                            padding:
-                                const EdgeInsets.only(left: 3.0, right: 7.0),
-                            decoration: BoxDecoration(
-                              color: ThemeStyle.buttonPrimaryColor,
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(30.0)),
-                            ),
-                            child: DropdownButton<int>(
-                              isDense: true,
-                              value: _routeManager.getGroupSize(),
-                              icon:
-                                  const Icon(Icons.group, color: Colors.white),
-                              iconSize: 30,
-                              elevation: 16,
-                              style: const TextStyle(
-                                  color: Colors.black45, fontSize: 18),
-                              menuMaxHeight: 200,
-                              onChanged: (int? newValue) =>
-                                  _routeManager.setGroupSize(newValue!),
-                              selectedItemBuilder: (BuildContext context) {
-                                return groupSizeOptions.map((int value) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 5.0),
-                                    child: Text(
-                                      _routeManager.getGroupSize().toString(),
-                                      style:
-                                          const TextStyle(color: Colors.white),
-                                    ),
-                                  );
-                                }).toList();
-                              },
-                              items: groupSizeOptions
-                                  .map<DropdownMenuItem<int>>((int value) {
-                                return DropdownMenuItem<int>(
-                                  value: value,
-                                  child: Text(value.toString()),
-                                );
-                              }).toList(),
-                            ),
-                          ),
+                          GroupSizeSelector(),
                           SizedBox(height: 10),
                           CircleButton(
                               iconIn: Icons.alt_route,
