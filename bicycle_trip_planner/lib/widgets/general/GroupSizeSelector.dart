a274900d41904185c2fa59268088a893f0fc5ffe
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-03-04 20:49:26 +0000

    Restyle group size selector
    - Remove underline
    - Add more padding

--- bicycle_trip_planner/lib/widgets/general/GroupSizeSelector.dart
+++ bicycle_trip_planner/lib/widgets/general/GroupSizeSelector.dart
@@ -23,44 +23,46 @@ class _GroupSizeSelectorState extends State<GroupSizeSelector> {
 
     return Container(
       padding:
-      const EdgeInsets.only(left: 3.0, right: 7.0),
+      const EdgeInsets.fromLTRB(5.0, 5.0, 10.0, 5.0),
       decoration: BoxDecoration(
         color: ThemeStyle.buttonPrimaryColor,
         borderRadius:
         const BorderRadius.all(Radius.circular(30.0)),
       ),
-      child: DropdownButton<int>(
-        isDense: true,
-        value: _routeManager.getGroupSize(),
-        icon:
-        const Icon(Icons.group, color: Colors.white),
-        iconSize: 30,
-        elevation: 16,
-        style: const TextStyle(
-            color: Colors.black45, fontSize: 18),
-        menuMaxHeight: 200,
-        onChanged: (int? newValue) =>
-            _routeManager.setGroupSize(newValue!),
-        selectedItemBuilder: (BuildContext context) {
-          return _groupSizeOptions.map((int value) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 5.0),
-              child: Text(
-                _routeManager.getGroupSize().toString(),
-                style:
-                const TextStyle(color: Colors.white),
-              ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<int>(
+          isDense: true,
+          value: _routeManager.getGroupSize(),
+          icon:
+          const Icon(Icons.group, color: Colors.white),
+          iconSize: 30,
+          elevation: 16,
+          style: const TextStyle(
+              color: Colors.black45, fontSize: 18),
+          menuMaxHeight: 200,
+          onChanged: (int? newValue) =>
+              _routeManager.setGroupSize(newValue!),
+          selectedItemBuilder: (BuildContext context) {
+            return _groupSizeOptions.map((int value) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 5.0),
+                child: Text(
+                  _routeManager.getGroupSize().toString(),
+                  style:
+                  const TextStyle(color: Colors.white),
+                ),
+              );
+            }).toList();
+          },
+          items: _groupSizeOptions
+              .map<DropdownMenuItem<int>>((int value) {
+            return DropdownMenuItem<int>(
+              value: value,
+              child: Text(value.toString()),
             );
-          }).toList();
-        },
-        items: _groupSizeOptions
-            .map<DropdownMenuItem<int>>((int value) {
-          return DropdownMenuItem<int>(
-            value: value,
-            child: Text(value.toString()),
-          );
-        }).toList(),
+          }).toList(),
+        ),
       ),
     );
   }
