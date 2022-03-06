84d8059f865ae630193cfc452bbed14ce562afd9
michaelmulianto <53204221+michaelmulianto@users.noreply.github.com>
2022-03-06 15:05:15 +0000

    fix directions managers test

--- bicycle_trip_planner/test/managers/direction_manager_test.dart
+++ bicycle_trip_planner/test/managers/direction_manager_test.dart
@@ -1,39 +1,45 @@
 import 'package:test/test.dart';
 import 'package:bicycle_trip_planner/managers/DirectionManager.dart';
+import 'package:bicycle_trip_planner/models/route.dart';
+import 'package:bicycle_trip_planner/models/bounds.dart';
+import 'package:bicycle_trip_planner/models/legs.dart';
+import 'package:bicycle_trip_planner/models/location.dart';
+import 'package:bicycle_trip_planner/models/steps.dart';
+import 'package:bicycle_trip_planner/models/overview_polyline.dart';
 
 void main(){
   final directionManager = DirectionManager();
 
   test('ensure isCycling is false when initialized', (){
-    expect(directionManager.isCycling, false);
+    expect(directionManager.ifCycling(), false);
   });
 
   test('ensure duration is no data when initialized', (){
-    expect(directionManager.duration, "No data");
+    expect(directionManager.getDuration(), "No data");
   });
 
   test('ensure distance is no data when initialized', (){
-    expect(directionManager.distance, "No data");
+    expect(directionManager.getDistance(), "No data");
   });
 
   test('ensure directions is empty when initialized', (){
-    expect(directionManager.directions.length, 0);
+    expect(directionManager.getDirections().length, 0);
   });
 
   test('ensure currentDirection is empty when initialized', (){
-    expect(directionManager.currentDirection.instruction, "");
-    expect(directionManager.currentDirection.duration, 0);
-    expect(directionManager.currentDirection.distance, 0);
+    expect(directionManager.getCurrentDirection().instruction, "");
+    expect(directionManager.getCurrentDirection().duration, 0);
+    expect(directionManager.getCurrentDirection().distance, 0);
   });
 
   test('test calculation conversion for duration is correct', (){
     directionManager.setDuration(60);
-    expect(directionManager.duration, '1 min');
+    expect(directionManager.getDuration(), '1 min');
   });
 
   test('test calculation conversion for distance is correct', (){
     directionManager.setDistance(1000);
-    expect(directionManager.distance, '1 km');
+    expect(directionManager.getDistance(), '1 km');
   });
 
   test('ensure that dummy directions are created', (){
@@ -41,11 +47,18 @@ void main(){
   });
 
   test('ensure that first direction and be removed and return next direction', (){
-    directionManager.directions = directionManager.createDummyDirections();
-    expect(directionManager.directions.length, 5);
-    final currentDirection = directionManager.directions[1];
+    final bounds = Bounds(northeast: {"a": 1}, southwest: {"b": 2});
+    final startLocation = Location(lat: 155.1, lng: -12.3);
+    final endLocation = Location(lat: 234.2, lng: -24.3);
+    final steps = Steps(instruction: "right", distance: 1, duration: 1);
+    final polyline = OverviewPolyline(points: []);
+    final legs = [Legs(startLocation: startLocation, endLocation: endLocation, steps: [steps], distance: 1, duration: 1), Legs(startLocation: startLocation, endLocation: endLocation, steps: [steps], distance: 2, duration: 2), Legs(startLocation: startLocation, endLocation: endLocation, steps: [steps], distance: 2, duration: 2)];
+    final route = Route(bounds: bounds, legs: legs, polyline: polyline);
+    directionManager.setCurrentRoute(route);
+    expect(directionManager.getNumberOfDirections(),2);
+    final currentDirection = directionManager.getDirections()[0];
     expect(directionManager.popDirection().instruction, currentDirection.instruction);
-    expect(directionManager.directions.length, 4);
+    expect(directionManager.getNumberOfDirections(), 1);
   });
 
 
