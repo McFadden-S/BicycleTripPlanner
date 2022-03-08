f3d2d4dc705a1720fbd5e760b64ae4419c58d9c1
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-03-08 15:35:17 +0000

    Add pop-up skeleton
    - Add 'Show dialog' button for testing purposes

--- bicycle_trip_planner/lib/widgets/home/Home.dart
+++ bicycle_trip_planner/lib/widgets/home/Home.dart
@@ -25,16 +25,78 @@ class _HomeState extends State<Home> {
           Align(
             alignment: Alignment.topCenter,
             child: Container(
-              color: Colors.grey.withOpacity(0.4),
-              child: SizedBox(height: topPaddingHeight, width: MediaQuery.of(context).size.width)
+                color: Colors.grey.withOpacity(0.4),
+                child: SizedBox(height: topPaddingHeight, width: MediaQuery.of(context).size.width)
             ),
           ),
           AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
               child: applicationBloc.getSelectedScreen()),
+          Align(
+            alignment: Alignment.center,
+            child: TextButton(
+              child: Text("Show dialog"),
+              onPressed: (){showMyDialog();},
+            ),
+          )
         ],
       ),
     );
   }
-}
 
+  void showMyDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius:
+                BorderRadius.circular(20.0)
+            ),
+            child: Container(
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 10),
+                      Expanded(child: Text("Set as:", textAlign: TextAlign.center)),
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size.fromWidth(double.infinity)
+                          ),
+                          onPressed: (){},
+                          child: Text("Starting point"),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Expanded(
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: Size.fromWidth(double.infinity)
+                            ),
+                            onPressed: (){},
+                            child: Text("Intermediate stop")
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Expanded(
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                minimumSize: Size.fromWidth(double.infinity)
+                            ),
+                            onPressed: (){},
+                            child: Text("Destination")
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                )
+            ),
+          );
+        });
+  }
+}
