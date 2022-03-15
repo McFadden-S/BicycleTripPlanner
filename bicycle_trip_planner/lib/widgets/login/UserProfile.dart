c6bb33984d6817cc101c48d4f9de8ec5c697a0c7
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-03-15 12:31:53 +0000

    Update UserProfile screen styling

--- bicycle_trip_planner/lib/widgets/login/UserProfile.dart
+++ bicycle_trip_planner/lib/widgets/login/UserProfile.dart
@@ -23,10 +23,11 @@ class _StationCardState extends State<UserProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ThemeStyle.cardColor,
-      body: Stack(
-        children: [
-          SafeArea(
-            child: Column(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Column(
               children: [
                 Align(
                   alignment: Alignment.topCenter,
@@ -40,16 +41,6 @@ class _StationCardState extends State<UserProfile> {
                   ),
                 ),
                 SizedBox(height: 30),
-                Text(
-                  _auth.currentUser?.email ?? "NO EMAIL",
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: ThemeStyle.primaryTextColor,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
                 Container(
                     margin: EdgeInsets.only(top: 5),
                     width: 60.0,
@@ -61,50 +52,58 @@ class _StationCardState extends State<UserProfile> {
                           fit: BoxFit.fill,
                           image: AssetImage('assets/profile.png'),
                         )
-                    ))
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Wrap(
-              children: [
-                Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 20),
-                        child: Column(children: [
-                          CircleButton(
-                            key: Key("Logout"),
-                            iconIn: Icons.logout,
-                            onButtonClicked: () async {
-                              await _auth.signOut();
-                              Navigator.pushReplacement(context,
-                                  MaterialPageRoute(builder: (context) {
-                                return AuthenticationScreen();
-                              }));
-                            },
-                          ),
-                          SizedBox(height: 10),
-                          CircleButton(
-                              key: Key("Back"),
-                              iconIn: Icons.arrow_back,
-                              onButtonClicked: () {
-                                Navigator.of(context, rootNavigator: true)
-                                    .pop(context);
-                              }),
-                        ]),
-                      ),
-                    ],
+                    )),
+                SizedBox(height: 10),
+                Text(
+                  _auth.currentUser?.email ?? "NO EMAIL",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: ThemeStyle.primaryTextColor,
                   ),
-                ]),
-                FavouriteBar()
+                ),
               ],
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Wrap(
+                children: [
+                  Column(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(bottom: 20),
+                          child: Column(children: [
+                            CircleButton(
+                              key: Key("Logout"),
+                              iconIn: Icons.logout,
+                              onButtonClicked: () async {
+                                await _auth.signOut();
+                                Navigator.pushReplacement(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return AuthenticationScreen();
+                                }));
+                              },
+                            ),
+                            SizedBox(height: 10),
+                            CircleButton(
+                                key: Key("Back"),
+                                iconIn: Icons.arrow_back,
+                                onButtonClicked: () {
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop(context);
+                                }),
+                          ]),
+                        ),
+                      ],
+                    ),
+                  ]),
+                  FavouriteBar()
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
