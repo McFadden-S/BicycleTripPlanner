af6065bcc249306ff02ea3b5c8d0787948d0e882
Sean Pan <sean.pan@kcl.ac.uk>
2022-03-18 14:40:32 +0000

    Wait for home so when loading is over, everything loads quickly

--- bicycle_trip_planner/lib/widgets/general/Loading.dart
+++ bicycle_trip_planner/lib/widgets/general/Loading.dart
@@ -2,6 +2,7 @@ import 'package:bicycle_trip_planner/managers/LocationManager.dart';
 import 'package:bicycle_trip_planner/managers/StationManager.dart';
 import 'package:bicycle_trip_planner/models/place.dart';
 import 'package:bicycle_trip_planner/widgets/general/MapWidget.dart';
+import 'package:bicycle_trip_planner/widgets/home/Home.dart';
 import 'package:bicycle_trip_planner/widgets/home/StationBar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -14,19 +15,18 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   void setUpMap() async {
     await MapWidget();
     await LocationManager().getCurrentLocation() != const Place.placeNotFound();
     await StationManager().getNumberOfStations() > 0;
     await StationBar();
-    await Future.delayed(Duration(seconds: 3));
-
+    await Home();
+    await Future.delayed(const Duration(seconds: 3));
     Navigator.pushReplacementNamed(context, '/home');
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     setUpMap();
   }
@@ -44,8 +44,3 @@ class _LoadingState extends State<Loading> {
     );
   }
 }
-
-
-
-
-
