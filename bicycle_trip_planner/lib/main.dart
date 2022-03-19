2a7593ee845344dda7a7a1ada50f475a2202517f
michaelmulianto <53204221+michaelmulianto@users.noreply.github.com>
2022-03-19 20:01:07 +0000

    delete unecessary information

--- bicycle_trip_planner/lib/main.dart
+++ bicycle_trip_planner/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/loading',
+      initialRoute: '/weather',
       routes: <String, WidgetBuilder>{
         // '/': (context) => const NavigateWindow(),
         '/settings': (context) => const SettingsScreen(),
