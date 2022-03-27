f54b70d9c9babab73948fa96cfd8e460488b0412
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2022-03-27 22:00:31 +0100

    Add animation to CustomBottomSheet

--- bicycle_trip_planner/lib/widgets/general/other/CustomBottomSheet.dart
+++ bicycle_trip_planner/lib/widgets/general/other/CustomBottomSheet.dart
@@ -15,80 +15,74 @@ class _StationCardState extends State<CustomBottomSheet> {
   bool isExpanded = true;
   @override
   Widget build(BuildContext context) {
-    return !isExpanded
-        ? Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              decoration: BoxDecoration(
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+            decoration: BoxDecoration(
                 color: ThemeStyle.cardColor,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0)),
-              ),
-              height: MediaQuery.of(context).size.height * 0.05,
-              child: Column(
-                children: [
-                  // top bar where the expand/shrink ion is
-                  Align(
-                      alignment: Alignment.topCenter,
-                      child: IconButton(
-                        padding: EdgeInsets.zero,
-                        constraints: BoxConstraints(),
-                        alignment: Alignment.topCenter,
-                        icon: Icon(Icons.keyboard_arrow_up,
-                            color: ThemeStyle.secondaryIconColor),
-                        tooltip: 'Expand',
-                        onPressed: () => {
-                          setState(() {
-                            isExpanded = true;
-                          })
-                        },
-                      )),
-                ],
-              ),
-            ),
-          )
-        : Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              decoration: BoxDecoration(
-                  color: ThemeStyle.cardColor,
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(30.0),
-                      topRight: Radius.circular(30.0)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: ThemeStyle.stationShadow,
-                      spreadRadius: 8,
-                      blurRadius: 6,
-                      offset: const Offset(0, 0),
-                    )
-                  ]),
-              child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.12,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Align(
-                          alignment: Alignment.topCenter,
-                          child: IconButton(
-                            padding: EdgeInsets.zero,
-                            constraints: BoxConstraints(),
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+                boxShadow: [
+                  BoxShadow(
+                    color: ThemeStyle.stationShadow,
+                    spreadRadius: 8,
+                    blurRadius: 6,
+                    offset: const Offset(0, 0),
+                  )
+                ]),
+          child: AnimatedSizeAndFade(
+              fadeDuration: const Duration(milliseconds: 300),
+              sizeDuration: const Duration(milliseconds: 300),
+              child: !isExpanded
+                  ? Container(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    child: Column(
+                      children: [
+                        // top bar where the expand/shrink ion is
+                        Align(
                             alignment: Alignment.topCenter,
-                            icon: Icon(Icons.keyboard_arrow_down,
-                                color: ThemeStyle.secondaryIconColor),
-                            tooltip: 'Shrink',
-                            onPressed: () => {
-                              setState(() {
-                                isExpanded = false;
-                              })
-                            },
-                          )),
-                      Expanded(child: widget.child),
-                    ],
+                            child: IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: BoxConstraints(),
+                              alignment: Alignment.topCenter,
+                              icon: Icon(Icons.keyboard_arrow_up,
+                                  color: ThemeStyle.secondaryIconColor),
+                              tooltip: 'Expand',
+                              onPressed: () => {
+                                setState(() {
+                                  isExpanded = true;
+                                })
+                              },
+                            )),
+                      ],
+                    ),
+                  ) : Container(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.12,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                                alignment: Alignment.topCenter,
+                                child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  constraints: BoxConstraints(),
+                                  alignment: Alignment.topCenter,
+                                  icon: Icon(Icons.keyboard_arrow_down,
+                                      color: ThemeStyle.secondaryIconColor),
+                                  tooltip: 'Shrink',
+                                  onPressed: () => {
+                                    setState(() {
+                                      isExpanded = false;
+                                    })
+                                  },
+                                )),
+                            Expanded(child: widget.child),
+                          ],
+                        )),
                   )),
-            ),
-          );
+        ));
   }
 }
