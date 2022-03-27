399caad0108cc5ec293756bdf7df7d33d0a651f3
Edward Reamer <edward.reamer@kcl.ac.uk>
2022-03-28 00:42:33 +0100

    Add more tests
    Add more tests

--- bicycle_trip_planner/lib/widgets/navigation/Navigation.dart
+++ bicycle_trip_planner/lib/widgets/navigation/Navigation.dart
@@ -123,7 +123,9 @@ class _NavigationState extends State<Navigation> {
                         Expanded(
                           child: Padding(
                             padding: const EdgeInsets.all(5.0),
-                            child: EndRouteButton(),
+                            child: EndRouteButton(
+                              key: Key('EndRouteButton'),
+                            ),
                           ),
                         ),
                         Expanded(
