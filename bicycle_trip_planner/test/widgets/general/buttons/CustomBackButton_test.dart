96fa02dd9a5b748191f6f93d23173ee781a25699
Edward Reamer <edward.reamer@kcl.ac.uk>
2022-03-27 18:04:59 +0100

    Create CustomBackButton_test.dart
    Create tests for CustomBackButton

---
+++ bicycle_trip_planner/test/widgets/general/buttons/CustomBackButton_test.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:bicycle_trip_planner/widgets/general/buttons/CustomBackButton.dart';
+import 'package:bicycle_trip_planner/widgets/routeplanning/RoutePlanning.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../setUp.dart';
+import '../../login/mock.dart';
+
+void main() {
+
+  setupFirebaseAuthMocks();
+
+  setUpAll(() async {
+    HttpOverrides.global = null;
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  });
+
+  testWidgets("CustomBackButton is a button", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: RoutePlanning()));
+    final BuildContext context = tester.element(find.byType(Container));
+    
+    await pumpWidget(tester, MaterialApp(home: Material(child: CustomBackButton(backTo: 'Navigation', context: context))));
+
+    final backButton = find.byType(CustomBackButton);
+
+    expect(backButton, findsOneWidget);
+  });
+
+  testWidgets("CircleButton has an icon", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: RoutePlanning()));
+    final BuildContext context = tester.element(find.byType(Container));
+
+    await pumpWidget(tester, MaterialApp(home: Material(child: CustomBackButton(backTo: 'Navigation', context: context))));
+
+    final icon = find.byIcon(Icons.arrow_back);
+
+    expect(icon, findsOneWidget);
+  });
+}
