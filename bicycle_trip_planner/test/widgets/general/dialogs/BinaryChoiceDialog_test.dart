dce4314798f1d98b096082f292e3b38dbb491402
Riccardo Gugliermini <riccardo.gugliermini@kcl.ac.uk>
2022-03-28 17:52:40 +0100

    Add tests for binary choice

--- bicycle_trip_planner/test/widgets/general/dialogs/BinaryChoiceDialog_test.dart
+++ bicycle_trip_planner/test/widgets/general/dialogs/BinaryChoiceDialog_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:bicycle_trip_planner/managers/DialogManager.dart';
 import 'package:bicycle_trip_planner/widgets/general/dialogs/BinaryChoiceDialog.dart';
 import 'package:bicycle_trip_planner/widgets/navigation/Navigation.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -16,6 +17,7 @@ void main() {
     HttpOverrides.global = null;
     TestWidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp();
+    DialogManager().showBinaryChoice();
   });
 
   testWidgets("Binary choice dialog contains two buttons", (WidgetTester tester) async {
@@ -33,17 +35,35 @@ void main() {
     //final binaryChoiceDialog = find.byType(BinaryChoiceDialog);
     // final button1 = find.descendant(of: find.byKey(ValueKey('Binary Button 1'), skipOffstage: false), matching: find.byType(ElevatedButton));
     // final button2 = find.descendant(of: find.byKey(ValueKey('Binary Button 2'), skipOffstage: false), matching: find.byType(ElevatedButton));
-    final text = find.text('Would you like to end your route?');
+    //final text = find.text('Would you like to end your route?');
+    final button1 = find.byKey(Key('Binary Button 1'));
+    final button2 = find.byKey(Key('Binary Button 2'));
 
     // var button1 = find.text('Yes', skipOffstage: false);
     // var button2 = find.text('No', skipOffstage: false);
 
-
     // expect(binaryButton, findsWidgets);
     //expect(binaryChoiceDialog, findsOneWidget);
-    expect(text, findsOneWidget);
+    expect(button1, findsOneWidget);
+    expect(button2, findsOneWidget);
     // expect(button1, findsOneWidget);
     // expect(button2, findsOneWidget);
 
+  });
+
+  testWidgets("Binary choice dialog is a dialog", (WidgetTester tester) async {
+    await pumpWidget(tester, MaterialApp(home: Material(child: BinaryChoiceDialog())));
+    final dialog = find.byType(Dialog);
+    expect(dialog, findsOneWidget);
+  });
+
+  testWidgets("Binary choice dialog contains some text", (WidgetTester tester) async {
+    DialogManager().setBinaryChoice("TEST DESCRIPTION", "option1", (){}, "option2", (){});
+    await pumpWidget(tester, MaterialApp(home: Material(child: BinaryChoiceDialog())));
+    final text = find.byType(Text).first;
+
+    expect(text, findsOneWidget);
+    Text s = text.evaluate().single.widget as Text;
+    expect(s.data, DialogManager().getChoicePrompt());
   });
 }
