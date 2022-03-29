e9b9345a68623ecfbaa4313f487b49c238bbe944
Alvin <alvinlam21@gmail.com>
2022-03-29 19:29:26 +0100

    add widget tests for FavouriteRouteCard.dart

--- bicycle_trip_planner/test/widgets/home/StationBar_test.dart
+++ bicycle_trip_planner/test/widgets/home/StationBar_test.dart
@@ -1,18 +1,26 @@
 import 'dart:io';
 
 import 'package:bicycle_trip_planner/widgets/home/StationBar.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import '../../managers/firebase_mocks/firebase_auth_mocks.dart';
 import '../../setUp.dart';
 
 void main() {
+  setupFirebaseAuthMocks();
+
   setUpAll(() async {
     HttpOverrides.global = null;
+    await Firebase.initializeApp();
   });
 
   testWidgets("StationBar has a title 'Nearby Stations'", (WidgetTester tester) async {
-    await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
-    expect(find.text('Nearby Stations'), findsOneWidget);
+    await tester.runAsync(
+            () async {
+              await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
+              expect(find.text('Nearby Stations'), findsOneWidget);
+        });
   });
 
   testWidgets("StationBar has an icon button 'firstPage'", (WidgetTester tester) async {
@@ -23,11 +31,6 @@ void main() {
   testWidgets("StationBar has an icon button 'expanded station list'", (WidgetTester tester) async {
     await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
     expect(find.widgetWithIcon(IconButton, Icons.menu), findsOneWidget);
-  });
-
-  testWidgets("StationBar has a stations listView", (WidgetTester tester) async {
-    await pumpWidget(tester, MaterialApp(home: Material(child:StationBar())));
-    expect(find.byType(ListView), findsOneWidget);
   });
 
 }
--- bicycle_trip_planner/test/widgets/home/stationCard_test.dart
+++ bicycle_trip_planner/test/widgets/home/stationCard_test.dart
@@ -36,37 +36,29 @@ void main() {
   });
 
   testWidgets("StationCard has a number of bikes displayed", (WidgetTester tester) async {
-    await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station,))));
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station))));
     expect(find.text("\t\t${station.bikes.toString()} bikes available"), findsWidgets);
   });
 
   testWidgets("StationCard has a number of docks displayed", (WidgetTester tester) async {
-    await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station,))));
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station))));
     expect(find.text("\t\t${station.totalDocks.toString()} free docks"), findsWidgets);
   });
 
-  // testWidgets("StationCard has a distance displayed", (WidgetTester tester) async {
-  //   await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station,))));
-  //   expect(find.text("${station.distanceTo.toStringAsFixed(1)}"), findsWidgets);
-  // });
-
-  testWidgets("StationBar has an icon directions_bike", (WidgetTester tester) async {
+  testWidgets("StationCard has an icon directions_bike", (WidgetTester tester) async {
     await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station))));
     expect(find.byIcon(Icons.directions_bike), findsWidgets);
   });
 
-  testWidgets("StationBar has an icon chair", (WidgetTester tester) async {
+  testWidgets("StationCard has an icon chair", (WidgetTester tester) async {
     await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station))));
     expect(find.byIcon(Icons.chair_alt), findsWidgets);
   });
 
-  // testWidgets("StationBar has favourite icon if favourite", (WidgetTester tester) async {
-  //   await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station, isFavourite: true))));
-  //   expect(find.byIcon(Icons.star), findsWidgets);
-  // });
+  testWidgets("StationCard has contents inside", (WidgetTester tester) async {
+    await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station))));
 
-  // testWidgets("StationBar has favourite icon if not favourite", (WidgetTester tester) async {
-  //   await pumpWidget(tester, MaterialApp(home: Material(child:StationCard(station: station, isFavourite: false))));
-  //   expect(find.byIcon(Icons.star), findsWidgets);
-  // });
+    expect(find.byType(InkWell), findsWidgets);
+  });
+
 }
