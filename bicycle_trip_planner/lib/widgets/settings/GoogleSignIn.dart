7c4dff16bd808dfcbe4d59cb4830d6d8d688d95a
Aadit Jain <aadit.jain@kcl.ac.uk>
2022-03-30 02:22:12 +0100

    Add comments

--- bicycle_trip_planner/lib/widgets/settings/GoogleSignIn.dart
+++ bicycle_trip_planner/lib/widgets/settings/GoogleSignIn.dart
@@ -2,26 +2,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+/// Class to let the user sign in through google
 class GoogleSignInProvider extends ChangeNotifier{
 
+  // Variables used to get allow sign in
   late var googleSignIn;
   late var _auth;
 
+  // Default constructor
   GoogleSignInProvider(){
     googleSignIn = GoogleSignIn();
 
     _auth = FirebaseAuth.instance;
   }
 
+  // Constructor used to pass in mocked values
   GoogleSignInProvider.forMock(GoogleSignIn signIn, FirebaseAuth auth){
     googleSignIn = signIn;
     _auth = auth;
   }
 
+  // The current user
   GoogleSignInAccount? _user;
-
   GoogleSignInAccount get user => _user!;
 
+  // Logs the user in using google
   Future googleLogin() async {
     final googleUser = await googleSignIn.signIn();
     if(googleUser == null) return;
@@ -35,6 +40,7 @@ class GoogleSignInProvider extends ChangeNotifier{
     notifyListeners();
   }
 
+  // Logs the user out
   Future logout() async {
     await googleSignIn.disconnect();
     _auth.signOut();
